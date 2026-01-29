db72e3958efa7313f59492782668ced86051c79e
ms-sadiya <sadiyaansari0003@gmail.com>
2026-01-29 21:15:25 +0530

    structure utils folder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("Mongodb connection failed!!");
+})
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResposne {
+  constructor(statusCode, data, message = 'Success') {
+    this.statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
